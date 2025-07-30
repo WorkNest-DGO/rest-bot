ce9174e23825dce3c544c12f56a6e915bd2c057f
WorkNest-DGO <78885137+WorkNest-DGO@users.noreply.github.com>
2025-07-30 13:43:31 -0600

    Log all incoming webhook payloads

--- webhook.js
+++ webhook.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const messageHandling = require('./messageHandling');
 
 const router = express.Router();
 
-router.post('/', messageHandling);
+router.post('/', (req, res) => {
+  fs.appendFileSync(
+    'debug_payload_log.txt',
+    new Date().toISOString() +
+      ' - Payload Entrante: ' +
+      JSON.stringify(req.body, null, 2) +
+      '\n'
+  );
+  return messageHandling(req, res);
+});
 
 module.exports = router;
